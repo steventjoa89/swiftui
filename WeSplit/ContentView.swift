f390375123438697614e68fa771a5b88299b43c2
steven.tjoa.tanudjaja <steven.tjoa.tanudjaja@geteasycash.asia>
2021-02-03 12:37:51 +0700

    Finishing WeSplit application.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,29 +1,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+	@State private var checkAmount = ""
+	@State private var numberOfPeople = 2
+	@State private var tipPercentage = 2
+	
+	let tipPercentages = [10, 15, 20, 25, 0]
+	
+	var totalPerPerson: Double {
+		// Calculate the total of person
+		let peopleCount = Double(numberOfPeople + 2)
+		let tipSelection = Double(tipPercentages[tipPercentage])
+		
+		// check apa save ga dari String ke double
+		// klo error, pake 0
+		let orderAmount = Double(checkAmount) ?? 0
+		let tipValue = orderAmount / 100 * tipSelection
+		let grandTotal = orderAmount + tipValue
+		let amountPerPerson = grandTotal / peopleCount
+		
+		return amountPerPerson
+	}
+		
+	var body: some View {
+		NavigationView {
 			Form {
-				Group {
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
+				Section {
+					TextField("Amount", text: self.$checkAmount)
+						.keyboardType(.decimalPad)
+					Picker("Number of people", selection: $numberOfPeople){
+						ForEach(2..<100){
+							Text("\($0) people")
+						}
+					}
 				}
-				Group {
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
-					Text("Hello, World!")
+				
+				Section(header: Text("How much tip do you to leave?")){
+					Picker("Tip percentage", selection: $tipPercentage){
+						ForEach(0..<self.tipPercentages.count){
+							Text("\(self.tipPercentages[$0])%")
+						}
+					}
+				.pickerStyle(SegmentedPickerStyle())
+				}
+				Section {
+					Text("$\(totalPerPerson, specifier: "%.2f")")
 				}
 			}
-    }
+		.navigationBarTitle("WeSplit")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
