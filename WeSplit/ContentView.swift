0b90201b51ada79020556327251c1185aac1332e
steven.tjoa.tanudjaja <steven.tjoa.tanudjaja@geteasycash.asia>
2021-02-03 11:36:42 +0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Steven Tjoa on 03/02/21.
+//  Copyright © 2021 Steven Tjoa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
